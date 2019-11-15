9bf1831be1740d0cd2e54b5b3f4d3c52982c39d9
Katerina <katerinalupacheva@gmail.com>
2019-11-15 13:39:34 +0300

    Shop data moved to firestore

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,30 +1,27 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-import CustomButton from '../custom-button/custom-button.component';
+// import CustomButton from '../custom-button/custom-button.component';
 import { addItem } from '../../redux/cart/cart.actions';
 
-import './collection-item.styles.scss';
+import './collection-item.styles';
+import { CollectionItemContainer, BackgroundImage, CollectionFooterContainer, NameContainer, PriceContainer, AddButton } from './collection-item.styles';
 
 const CollectionItem = ({ item, addItem }) => {
     const { name, price, imageUrl } = item;
     return (
-    <div className='collection-item'>
-        <div 
-            className='image'
-            style={{
-                backgroundImage: `url(${imageUrl})`
-            }}
-        
+    <CollectionItemContainer>
+        <BackgroundImage 
+            className='image' imageUrl={imageUrl}        
         />
-        <div className='collection-footer'>
-            <span className='name'>{name}</span>
-            <span className='price'>{price}</span>
-        </div>
-        <CustomButton onClick={() => addItem(item)} inverted>
+        <CollectionFooterContainer>
+            <NameContainer>{name}</NameContainer>
+            <PriceContainer>{price}</PriceContainer>
+        </CollectionFooterContainer>
+        <AddButton className='button' onClick={() => addItem(item)} inverted>
             Add to Cart
-        </CustomButton>
-    </div>
+        </AddButton>
+    </CollectionItemContainer>
     )};
 
 const mapDispatchToProps = dispatch => ({
