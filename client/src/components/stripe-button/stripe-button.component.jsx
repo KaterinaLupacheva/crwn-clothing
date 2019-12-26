cb695f8d5cf35136eaeb78ee7f778225e8ec52fd
Katerina <katerinalupacheva@gmail.com>
2019-12-26 23:20:05 +0300

    Backend added

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
 
 import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
 
 const StripeCheckoutButton = ({ price }) => {
     const priceForStripe = price * 100;
     const publishableKey = 'pk_test_J5MxMKJbk2JfoHZVNivkhVi500UZzcYCmm';
     const onToken = token => {
-        console.log(token);
-        alert('Payment Successful')
+        axios({
+            url: 'payment',
+            method: 'post',
+            data: {
+                amount: priceForStripe,
+                token
+            }
+        }).then(response => {
+            alert('Payment successful')
+        }).catch(error => {
+            console.log('Payment error: ', JSON.parse(error));
+            alert('Issue with payment');
+        })
     };
 
     return (
